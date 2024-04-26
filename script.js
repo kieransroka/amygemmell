c81ce951daaab9c25aa9ea1fdbe2d37aadac7091
kieransroka <amy.gemmell@hotmail.co.uk>
2024-04-26 20:16:08 +0100

    Portfolio added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// For nav animation
 let links = document.getElementsByClassName("nav-link");
 let activePage = [];
 
@@ -22,3 +23,59 @@ for (let i = 0; i < links.length; i++) {
         activePage[0].classList.add("active-link");
     })
 }
+
+//For dropdown
+if (document.getElementById("category-select")) {
+    let dropdown = document.getElementById("category-select");
+    let droplist = document.getElementById("dropdown-menu");
+    let portContains = document.getElementsByClassName("port-contain");
+    dropdown.addEventListener("show.bs.dropdown", function () {
+        droplist.classList.add("transition-down");
+    })
+    dropdown.addEventListener("hide.bs.dropdown", function () {
+        droplist.classList.remove("transition-down");
+    })
+
+    let droplink = document.getElementsByClassName("dropdown-item");
+    for (let i = 0; i < droplink.length; i++) {
+        const e = droplink[i];
+        e.addEventListener("click", function () {
+            let page = e.dataset.page;
+            switch (page) {
+                case "textiles":
+                    for (let i = 0; i < portContains.length; i++) {
+                        const e = portContains[i];
+                        if (e.id === page) {
+                            e.style.display = "block";
+                        } else {
+                            e.style.display = "none";
+                        }
+                    }
+                    break;
+
+                case "collage":
+                    for (let i = 0; i < portContains.length; i++) {
+                        const e = portContains[i];
+                        if (e.id === page) {
+                            e.style.display = "block";
+                        } else {
+                            e.style.display = "none";
+                        }
+                    }
+                    break;
+
+                default:
+                    for (let i = 0; i < portContains.length; i++) {
+                        const e = portContains[i];
+                        if (e.id === portSelected) {
+                            e.style.display = "block";
+                        } else {
+                            e.style.display = "none";
+                        }
+                    }
+                    break;
+            }
+        })
+    }
+}
+
